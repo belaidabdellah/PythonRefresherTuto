fe8e47c1103065f51512094b051504d52c7bd467
belaidabdellah <belaid90@gmail.com>
2021-09-22 23:24:22 +0200

    ajout d'un message pour le nombre de coups nécessaires

--- bouclespython/boucles.py
+++ bouclespython/boucles.py
@@ -1,4 +1,4 @@
-# importation du randint
+property# importation du randint
 from random import randint
 
 # choisir un nombre aleatoire entre 1 et 1000
@@ -6,7 +6,7 @@ just_price = randint(1, 1000)
 
 # statut du jeu (activé/désactivé)
 running = True
-
+coup =1
 # demander à l'utilisateur d'entrer un prix dans la console
 user_price = int(input("Entrer un prix entre 1 & 1000 : "))
 
@@ -16,15 +16,18 @@ while running:
     # si le prix est le meme que le juste prix
     if user_price == just_price:
         print("Trouvé !")
+        print("tu as eu besoin de {} coups".format(coup))
         # fin du jeu
         running = False
 
     # si le prix de l'utilisateur est supérieur au prix à trouver
     elif user_price > just_price:
+        coup+=1
         print("C'est moins")
 
     # si le prix de l'utilisateur est inférieur au prix à trouver
     elif user_price < just_price:
+        coup+=1
         print("C'est plus")
 
     # demander à l'utilisateur d'entrer un prix dans la console si le prix n'est toujours pas correct
